c36f7105075e11df72d87ed163856aa25748d712
yeejiac <yeejiac@gmail.com>
2021-07-09 22:39:26 -0400

    add RandomlyAssigned mechanism

--- models/user.go
+++ models/user.go
@@ -1,18 +1,21 @@
 package models
 
 type User struct {
-	Username   string `json:"username"`
-	Name       string `json:"name"`
-	Age        int    `json:"age"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-	Validation bool   `json:"vaidation"`
+	Username          string        `json:"username"`
+	Name              string        `json:"name"`
+	Age               int           `json:"age"`
+	Email             string        `json:"email"`
+	Password          string        `json:"password"`
+	Validation        bool          `json:"vaidation"`
+	Address           string        `json:"address"`
+	TotalExchangeTime int           `json:"totalexchangetime"`
+	KKC               int           `json:"kkc"`
+	Status            AccountStatus `json:"Status"`
 }
 
-type Privacy int
+type AccountStatus int
 
 const (
-	PUBLIC Privacy = iota
-	PRIVATE
-	PROTECT
+	ENABLE = iota
+	DISABLE
 )
